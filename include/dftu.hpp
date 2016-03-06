302957f62adff7a00888621211ff5f53011be7f2
triblatron <tony.horrobin@gmail.com>
2016-03-06 15:34:25 +0000

    Derived units
    First attempt at derived units.
    Fix syntax.
    Add base() to determine whether a dimension is fundamental.
    Remove const from dimension since it is not required.

--- include/dftu.hpp
+++ include/dftu.hpp
@@ -15,28 +15,42 @@ enum BaseDimension
 	BASEDIM_NONE,
 	//! Pseudo-dimension to use as a denominator in a derived dimension.
 	BASEDIM_ONE,
-	//! Length dimension (m).
+	//! Spatial dimension (L).
 	BASEDIM_LENGTH,
-	//! Time dimension (s).
+	//! Time dimension (T).
 	BASEDIM_TIME,
-	//! Mass dimension (kg).
+	//! Mass dimension (M).
 	BASEDIM_MASS,
+	//! Electric charge (Q).
+	BASEDIM_CHARGE,
+	//! Absolute Temperature (Theta).
+	BASEDIM_TEMPERATURE,
+	//! Amount of a substance (N).
+	BASEDIM_AMOUNT,
 	//! Dummy last dimension, use as a limit.
 	BASEDIM_MAX
 };
 
 //! Derived dimensions.
-//! A polynomial of base dimensions.
-struct DFTU_API DerivedDimension
+//! A arary of powers of base dimensions.
+//! A base dimension is one where only one element 
+//! is non-zero and is equal to one.
+struct DFTU_API Dimension
 {
-	int power[BASEDIM_MAX]
+	int power[BASEDIM_MAX];
 };
 
-//! Pre-defined derived dimensions for velocity.
-extern const DFTU_API DerivedDimension DIM_VELOCITY;
-//! Pre-defined derived dimensions for density.
-extern const DFTU_API DerivedDimension DIM_DENSITY;
+//! Determine whether a given dimension is a base dimension.
+//! @retval true if dim is a base dimension.
+//! @retval false otherwise.
+extern bool DFTU_API base( const Dimension & dim );
 
+//! Pre-defined derived dimensions for velocity.
+extern const DFTU_API Dimension DIM_VELOCITY;
+//! Pre-defined derived dimensions for density.
+extern const DFTU_API Dimension DIM_DENSITY;
+
+//! A base unit, defined for one of the base quantities.
 struct BaseUnit
 {
 	//! Name of the unit e.g. 'metre'
@@ -44,15 +58,17 @@ struct BaseUnit
 	//! Symbol for the unit e.g. 'm'
 	const char * symbol;
 	//! The dimension of the unit.
-	const BaseDimension dimension;
+	BaseDimension dimension;
 };
 
 extern const DFTU_API BaseUnit UNIT_METRE;
 extern const DFTU_API BaseUnit UNIT_FOOT;
 extern const DFTU_API BaseUnit UNIT_SECOND;
 extern const DFTU_API BaseUnit UNIT_KILOGRAM;
+extern const DFTU_API BaseUnit UNIT_RADIAN;
 
 //! The base 10 power of a unit.
+//! This defines some useful presets without restricting the value.
 enum Scale
 {
 	SCALE_MICRO=-6,
@@ -60,17 +76,29 @@ enum Scale
 	SCALE_CENTI=-2
 	SCALE_DECI=-1,
 	SCALE_KILO=3,
-	SCALE_MEGA=6
+	SCALE_MEGA=6,
+	SCALE_TERA=9
 };
 
-struct DerivedUnit
+//! A combination of base units.
+struct DFTU_API DerivedUnit
 {
 	//! The name of the unit e.g. millemetre.
 	const char * name;
+	//! The ASCII symbol of the unit e.g. 'mm'.
+	const char * symbol;
 	//! The base unit we are modifying.
-	const BaseUnit baseUnit;
+	const BaseUnitPower * combination;
+	//! The base 10 power of the unit.
 	//! This turns e.g. m into mm.
-	Scale scale;
+	unsigned char scale;
+};
+
+struct DFTU_API Quantity
+{
+	//! The name of the quantity.
+	const char * name;
+	const DerivedUnit * unit;
 };
 
 }
